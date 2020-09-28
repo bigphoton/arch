a5febadc4907951630872f855ab6f3d2c38dd78c
Lawrence Rosenfeld <lawrencerosenfeld@Lawrences-MacBook-Pro.local>
2020-09-28 16:14:00 +0100

    Improved commenting

--- test/validate.py
+++ test/validate.py
@@ -1,14 +1,12 @@
-'''
-Created on 28 Sep 2020
-
-@author: lawrencerosenfeld
-'''
-
 import unittest
 from os.path import dirname
 
 def main():
-	# Look for all unittests.TestCases in this folder, with a prefix of *_test.py
+	'''
+	This script runs all unit tests associated with the arch project 
+	'''
+	
+	# Look for all unittests.TestCases in this folder, with a prefix of test_*.py
 	path = dirname(__file__)
 	suite = unittest.TestLoader().discover(path)
 
