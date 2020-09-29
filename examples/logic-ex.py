f2c8af71ebd7b43d1e075b89730aacf28ddab619
Josh Silverstone <josh.silverstone@bris.ac.uk>
2020-09-29 16:35:36 +0100

    Added basic logic gates

--- examples/logic-ex.py
+++ examples/logic-ex.py
@@ -8,9 +8,9 @@ sys.path.insert(0,'..')
 
 from arch.blocks import electronics
 
+# AND - NOT
 
-
-first_gate = electronics.not_gate()
+first_gate = electronics.and_gate()
 first_gate.position = (-200,+20)
 
 second_gate = electronics.not_gate()
@@ -20,11 +20,48 @@ second_gate.position = (+200,+20)
 first_gate.ports['OUT'].connect(second_gate.ports['IN'])
 
 
-for input_val in [0,1]:
-	first_gate.ports['IN'].value = input_val
+print("{:6} -> {:}".format("input","!AND"))
+for a,b in [(0,0),(0,1),(1,0),(1,1)]:
+	first_gate.ports['IN0'].value = a
+	first_gate.ports['IN1'].value = b
 	
 	first_gate.compute()
 	second_gate.compute()
 	
-	print("gate1 in={:}, output={:}".format(input_val,first_gate.out_ports))
-	print("gate2 in={:}, output={:}".format(input_val,second_gate.out_ports))
+	print("{:} -> {:}".format((a,b), second_gate.ports['OUT'].value))
+
+
+# NAND - NOT
+
+first_gate = electronics.nand_gate()
+second_gate = electronics.not_gate()
+
+first_gate.ports['OUT'].connect(second_gate.ports['IN'])
+
+print("{:6} -> {:}".format("input","!NAND"))
+for a,b in [(0,0),(0,1),(1,0),(1,1)]:
+	first_gate.ports['IN0'].value = a
+	first_gate.ports['IN1'].value = b
+	
+	first_gate.compute()
+	second_gate.compute()
+	
+	print("{:} -> {:}".format((a,b), second_gate.ports['OUT'].value))
+
+
+# OR - NOT
+
+first_gate = electronics.or_gate()
+second_gate = electronics.not_gate()
+
+first_gate.ports['OUT'].connect(second_gate.ports['IN'])
+
+print("{:6} -> {:}".format("input","!OR"))
+for a,b in [(0,0),(0,1),(1,0),(1,1)]:
+	first_gate.ports['IN0'].value = a
+	first_gate.ports['IN1'].value = b
+	
+	first_gate.compute()
+	second_gate.compute()
+	
+	print("{:} -> {:}".format((a,b), second_gate.ports['OUT'].value))
