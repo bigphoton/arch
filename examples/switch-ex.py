97fded4fdb4aa5ed82624ea3404a28301de71443
Josh Silverstone <josh@qontrol.co.uk>
2020-10-30 13:15:45 +0000

    Tidying

--- examples/switch-ex.py
+++ examples/switch-ex.py
@@ -3,15 +3,15 @@ Example for switches.
 """
 
 # Add the parent folder (outside of `examples`) to the path, so we can find the `arch` module
-import sys
-sys.path.insert(0,'..')
+import sys, os
+sys.path.append(os.path.dirname(sys.path[0]))
 
 from arch.blocks import electro_optics
 from arch.blocks import optics
 
 import numpy as np
 	
-print ("Hello world")
+print ("Switch example start...")
 
 
 
@@ -26,9 +26,10 @@ bs.ports['OUT0'].connect(switch.ports['IN0'])
 bs.ports['IN0'].value = 1.0
 bs.ports['IN1'].value = 0.0
 
+
 for switch_state in [0, 1]:
 	switch.ports['DIG'].value = switch_state
 	bs.compute()
 	switch.compute()
 	out_ports = [bs.out_ports["OUT1"], switch.ports["OUT0"], switch.ports["OUT1"] ]
-	print("switch state={:}, output={:}".format(switch_state, out_ports))
+	print("switch state={:}, output={:}".format(switch_state, [p.value for p in out_ports]))
