33b6012133653c8177731d579643dabd802f1f05
Josh Silverstone <josh.silverstone@bris.ac.uk>
2021-02-09 19:08:35 +0000

    New example for time-independent optics

--- examples/timeless_optics.py
+++ examples/timeless_optics.py
@@ -15,39 +15,62 @@ from arch.blocks.optics import Beamsplitter, PhaseShifter, MachZehnder
 
 if __name__ == '__main__':
 	
-	ps = PhaseShifter()
-	bs = Beamsplitter()
+	ps0 = PhaseShifter()
+	ps1 = PhaseShifter()
+	ps2 = PhaseShifter()
+	bs0  = Beamsplitter()
 	bs1 = Beamsplitter()
 	
 	con_mz = Connectivity( [
-					(bs.out1, ps.inp),
-					(ps.out, bs1.in1),
-					(bs.out0, bs1.in0),
+					(ps0.out, bs0.in0),
+					(bs0.out1, ps1.inp),
+					(ps1.out, bs1.in1),
+					(bs0.out0, bs1.in0),
 					] )
 	
 	con_bs_only = Connectivity( [
-					(bs.out1, ps.inp),
-					(ps.out, bs1.in1),
-					(bs1.out1, bs.in1),
+					(ps0.out, bs0.in0),
+					(bs0.out1, ps1.inp),
+					(ps1.out, bs1.in1),
+					(bs1.out1, bs0.in1),
 					] )
 	
 	con_ring = Connectivity( [
-					(bs.out1, ps.inp),
-					(ps.out, bs.in1),
+					(ps0.out, bs0.in0),
+					(bs0.out1, ps1.inp),
+					(ps1.out, bs0.in1),
 					] )
 	
 	con_double_ring = Connectivity( [
-					(bs.out1, ps.inp),
-					(ps.out, bs1.in1),
-					(bs1.out1, bs.in1),
-					(bs1.out0, bs1.in0),
+					(ps0.out, bs0.in0),
+					(bs0.out1, ps1.inp),
+					(ps1.out, bs1.in1),
+					(bs1.out1, bs0.in1),
+					(bs1.out0, ps2.inp),
+					(ps2.out, bs1.in0),
 					] )
 	
-	con = con_ring
+	cons = [(con_double_ring,'double ring'), (con_mz,'mz'), 
+			(con_bs_only,'bs only'), (con_ring,'ring')]
 	
-# 	con.draw()
+	for con, name in cons:
+		
+		print('|'*80 + f"\n\nConnectivity '{name}'")
+# 		con.draw()
+		
+		print("Compounding")
+		cm = Linear.compound("my compound", con.models, con)
+		
+		if type(cm) == Linear:
+			print("Linear: Success!")
+		else:
+			print(f"{type(cm)}: Failure...")
+			quit()
+		
+		print(f"and it has this unitary matrix:")
+		from sympy import pprint
+		pprint(cm.U)
 	
-	cm = Linear.compound("compound name", con.models, con)
 	
 	
 	
