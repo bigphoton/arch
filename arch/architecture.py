776dae39809014b3aba57985805c899830e5d995
David Roberts <daveroberts1994@live.co.uk>
2021-05-19 13:10:25 +0100

    Refactored file according to flake8 formatting and added type hints

--- arch/architecture.py
+++ arch/architecture.py
@@ -1,42 +1,43 @@
+from typing import List, Iterable
 
-
+from arch.simulations.simulations import Simulator
+from arch.block import Block
+from arch.connectivity import Connectivity
 
 
 class Architecture(object):
-	
-	def __init__(self, blocks=None, connections=None, simulation=None):
-		
-		self.__blocks = blocks
-		self.__connections = connections
-		self.__simulations = [simulation]
-	
-	
-	@property
-	def connections(self):
-		return self.__connections
-	
-	@connections.setter
-	def connections(self, new):
-		self.__connections = new
-	
-	
-	@property
-	def simulations(self):
-		return self.__simulations
-	
-	@connections.setter
-	def simulations(self, new):
-		self.__simulations = new
-	
-	
-	@property
-	def blocks(self):
-		return self.__blocks
-	
-	
-	def add_connections(self, connections):
-		self.__connections.append(connections)
-	
-	
-	def add_simulation(self, simulation):
-		self.__simulations.append(simulation)
+
+    def __init__(self,
+                 blocks: Iterable[Block] = None,
+                 connections: Connectivity = None,
+                 simulation: Simulator = None) -> None:
+
+        self.__blocks = blocks
+        self.__connections = connections
+        self.__simulations = [simulation]
+
+    @property
+    def connections(self) -> Connectivity:
+        return self.__connections
+
+    @connections.setter
+    def connections(self, new) -> None:
+        self.__connections = new
+
+    @property
+    def simulations(self) -> List[Simulator]:
+        return self.__simulations
+
+    @connections.setter
+    def simulations(self, new: List[Simulator]) -> None:
+        self.__simulations = new
+
+    @property
+    def blocks(self) -> Iterable[Block]:
+        return self.__blocks
+
+    def add_connections(self, connections) -> None:
+        self.__connections.append(connections)
+
+    def add_simulation(self, simulation: Simulator) -> None:
+        self.__simulations.append(simulation)
