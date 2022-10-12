018e05cdfe6eb7be7ceefded70ff218cffe3ad5e
ja0877 <ja0877@bristol.ac.uk>
2022-10-12 14:32:39 +0100

    re-implementation of brandon's project B by JCA

--- raw/quantum_model/q-states2.py
+++ raw/quantum_model/q-states2.py
@@ -26,8 +26,10 @@ from arch.blocks.wire import Wire
 from arch.architecture import Architecture
 from arch.simulations import length_to_ps, ps_to_length, ng_fibre, ng_siwg, ng_TL, get_delay_map, BasicDynamicalSimulator
 import networkx as nx
-
-
+import thewalrus as tw
+import numpy as np
+from collections import defaultdict
+import copy
 # from arch.raw.quantum_model.quantum_model import *
 
 
@@ -49,13 +51,193 @@ if __name__=='__main__':
     # print(f"Initial: t={st.integral_t()}, t2={st.integral_t2()}")
     # print(f"Initial: f={st.integral_f()}, f2={st.integral_f2()}")
      # abs. sqaure not equal
+    alpha = 1/np.sqrt(3)
+    beta = 1j/np.sqrt(3)
+    gamma = 1j/np.sqrt(3)
 
 
-
-    # qstate = [{'modes' : ['wg_1', 'wg_2'], 'pos' : [1, 1], 'occ' : [1,1]},
-              # {'modes' : ['wg_1', 'wg_2'], 'pos' : [1, 1], 'occ' : [0,2]},
-              # {'modes' : ['wg_1', 'wg_2'], 'pos' : [1, 1], 'occ' : [0,2]}]
+    myqstate = [{'amp': alpha, 'modes' : ['wg_1', 'wg_2'], 'pos' : [1, 1], 'occ' : [1,1]},
+                {'amp': beta,  'modes' : ['wg_1'],         'pos' : [1],    'occ' : [2]  },
+                {'amp': gamma, 'modes' : ['wg_2'],         'pos' : [1],    'occ' : [2]  }]
               
+              
+    myU = 1/np.sqrt(2)*np.array([[1,1j],[1j,1]])
+    myU_mode_order = ['wg_1', 'wg_2']
+    
+    
+    def genfockslist_core(kk, nn):
+        """ https://stackoverflow.com/questions/7748442/generate-all-possible-lists-of-length-n-that-sum-to-s-in-python
+        """
+        if kk == 1:
+            yield [nn,]
+        else:
+            for value in range(nn + 1):
+                for permutation in genfockslist_core(kk - 1, nn - value):
+                    yield [value,] + permutation
+                
+    def genfockslist(k, n):
+        """
+        generates fock states with n photons in k modes
+        """                        
+        ans = list(genfockslist_core(k, n))
+        ans.reverse()
+        ans = np.array(ans)
+        return ans
+    
+    
+    
+    def gendictoutputs(wg_mode_names, focks, outputpos):
+        """
+        generates dictionary of fock modes for given set of waveguide modes and photon number n
+        """
+        dict = {}
+        occs = []
+        outpdict_list = []
+        for fock in focks:
+            fock_occ = [num for num in fock if num]
+            occs.append(fock_occ)
+            
+            fock_pos = np.nonzero(fock > 0)[0]
+            outpdict_list.append( {'modes' : [wg_mode_names[index] for index in fock_pos], 'occ' : fock_occ, 'pos' : [outputpos for i in range(len(fock_occ))]  }  )
+        # print(outpdict_list)
+        return outpdict_list
+        # for fock in focks
+            
+    
+
+    
+
+     
+    def q_simplify (qstate):
+        """
+        adds together wavevectors with the same coordinates - does quantum inteference
+        """
+        qstate_out = []
+        my_coords = []
+        sets = {}
+
+        for idx,vec in enumerate(qstate) :
+        
+            coord_keys = list(vec.keys())
+            coord_keys.remove('amp')
+            coords = [vec[coord_keys[k]] for k in range(len(coord_keys))]
+
+            if coords not in my_coords :
+                my_coords.append(coords)
+                sets[str(my_coords[-1])] =  [idx]
+            else :
+                new_val=list(sets[str(coords)])
+                new_val.append(idx)
+                sets[str(coords)] = new_val
+                
+        setslist = list(sets.values())
+
+        qstate_out = [0 for i in range(len(setslist))]
+        for i in range(len(setslist)):
+            qstate_out[i] = copy.deepcopy(qstate[setslist[i][0]])
+            qstate_out[i]['amp'] = 0.
+            for idx in setslist[i]:
+                qstate_out[i]['amp'] += qstate[idx]['amp']
+
+
+        return qstate_out    
+        
+    
+    def qcruncher(qstate, U, myU_modes, outputfocks = None):
+        """
+        takes in fock states and a unitary and computes the result
+        """
+        inp_norm = 0
+        for vec in qstate:
+            inp_norm += np.abs(vec['amp'])**2
+        print("L-2 norm of input was: {}".format(inp_norm),"\n")
+        
+        outputstate = []
+        
+        vecmodes = []
+        for vec in qstate:
+            vecmodes += vec['modes']
+            vecmodes = list(dict.fromkeys(vecmodes))
+        
+        if not (set(myU_modes).issubset(set(vecmodes))):
+            print("error! unequal modes in state and U")
+        
+        if outputfocks == None:
+            outputfocks = gendictoutputs(my_modes, genfockslist(2,2), outputpos = 2)
+            
+        for ivec in qstate:   
+            subU_iidx = []
+            i=0
+            for imode in ivec['modes']:
+                if imode in myU_modes:            # get index of mode for submatrix extraction
+                    subU_iidx.append(myU_modes.index(imode))
+                    for j in range(ivec['occ'][i]-1):            # handle repeated row/columns
+                            subU_iidx.append(myU_modes.index(imode))
+                   
+            for ovec in outputfocks:
+                subU_oidx = []
+                i=0
+                for omode in ovec['modes']:
+                    if omode in myU_modes:
+                        subU_oidx.append(myU_modes.index(omode))
+                        for j in range(ovec['occ'][i]-1):            # handle repeated row/columns
+                            subU_oidx.append(myU_modes.index(omode))
+                           
+       
+                subU = U[np.ix_(subU_iidx,subU_oidx)]
+                
+                denom1 = np.prod(list(map(np.math.factorial, ovec['occ'])))
+                denom2 = np.prod(list(map(np.math.factorial, ivec['occ'])))
+                trans_amp = tw.perm(subU)/np.sqrt(denom1*denom2)
+                
+                newamp = ivec['amp']*trans_amp
+                
+                if newamp != 0:
+                    outputstate.append({'amp' : newamp, 'modes' : omode, 'occ' : ovec['occ'], 'pos' : ovec['pos']})
+        
+
+        outp_norm = 0
+        for state in outputstate:
+            outp_norm += np.abs(state['amp'])**2 
+        print("L-2 norm of output was: {:}".format(outp_norm),"\n")   
+
+        outputstate = q_simplify(outputstate)
+                
+        return outputstate
+        
+
+        
+        
+        
+        
+ 
+    my_modes = ['wg_1', 'wg_2']
+    res = qcruncher(myqstate, myU, my_modes)
+
+    inp_norm = 0
+    for state in res:
+        inp_norm += np.abs(state['amp'])**2
+        
+    print("L-2 norm of output was: {}".format(inp_norm),"\n")
+        
+    print('')
+    print('')
+    print('SUCCESS')
+    print('')
+    print('')
+    
+    quit() 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     print ("Welcome to the new, NEW arch")
 
     components = []
